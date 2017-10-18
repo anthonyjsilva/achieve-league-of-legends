4e23b4828633f2bec82336c098ea7abba2c9825a
Anthony Silva <1xajsilva@gmail.com>
2017-10-18 15:07:57 -0500

    added node-fetch for server-side http requests, got basic page to populate from recieved json data

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 const API_KEY = require('./apikey');
+const URL = 'https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/fiesta%20clown?api_key=' + API_KEY;
 
 const express = require('express');
+const fetch = require('node-fetch');
+const mustacheExpress = require('mustache-express');
+// const bodyParser = require('body-parser');
+
 const app = express();
 
+app.engine('mustache', mustacheExpress());
+app.set('views', './views')
+app.set('view engine', 'mustache')
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   console.log(API_KEY);
-  res.send('Hello World!');
+
+  fetch(URL).then(function(res) {
+    return res.json();
+  }).then(function(json) {
+    console.log(json);
+    res.render('index', json);
+  });
+
 });
 
 app.listen(3000, () => {
