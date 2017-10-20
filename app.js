9782af9861c8fce07b908cead6d80e928da639dc
Anthony Silva <1xajsilva@gmail.com>
2017-10-20 14:59:30 -0500

    got match history api endpoint to populate on page

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const API_KEY = require('./apikey');
-const URL = 'https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/';
+
+const API_URL = 'https://na1.api.riotgames.com/lol/';
+const GET_SUMMONER_URL = 'summoner/v3/summoners/by-name/';
+const GET_MATCHES_URL ='match/v3/matchlists/by-account/215961083/recent'
 
 const express = require('express');
 const fetch = require('node-fetch');
@@ -23,13 +26,21 @@ app.get('/', (req, res) => {
 
 app.post('/search', (req, res) => {
   console.log(req.body);
+  const data = {};
 
-  fetch(`${URL}${req.body.name}?api_key=${API_KEY}`)
+  fetch(`${API_URL}${GET_SUMMONER_URL}${req.body.name}?api_key=${API_KEY}`)
     .then(res => res.json())
-      .then(json => {
-        console.log(json);
-        res.render('search', json);
-      });
+    .then(json => {
+      data.summoner = json;
+      fetch(`${API_URL}${GET_MATCHES_URL}?api_key=${API_KEY}`)
+        .then(res => res.json())
+        .then(json => {
+          console.log(json);
+          data.matches = json;
+          console.log(data);
+          res.render('search', data);
+        });
+    });
 });
 
 app.listen(3000, () => console.log('Successfully started express application!'));
