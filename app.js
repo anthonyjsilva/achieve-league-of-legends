f0192f900b6cdfa4ec9d51de0e134b144c208b69
Anthony Silva <1xajsilva@gmail.com>
2017-10-23 15:48:48 -0500

    cleaned up code, added in match endpoint

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const API_KEY = require('./apikey');
 
-const API_URL = 'https://na1.api.riotgames.com/lol/';
-const GET_SUMMONER_URL = 'summoner/v3/summoners/by-name/';
-const GET_MATCHES_URL ='match/v3/matchlists/by-account/215961083/recent';
+const BASE_API_URL = 'https://na1.api.riotgames.com/lol/';
+const SUMMONER_ENDPOINT = 'summoner/v3/summoners/by-name/';
+const GET_MATCHES_URL = 'match/v3/matchlists/by-account/215961083/recent';
+const CHAMPIONS_API_URL = 'static-data/v3/champions';
 
-const express = require('express');
-const fetch = require('node-fetch');
-const mustacheExpress = require('mustache-express');
-const bodyParser = require('body-parser');
 
-const app = express();
+const express = require('express'),
+  fetch = require('node-fetch'),
+  mustacheExpress = require('mustache-express'),
+  bodyParser = require('body-parser'),
+  app = express();
 
 app.engine('mustache', mustacheExpress());
 app.set('view engine', 'mustache')
@@ -19,22 +20,19 @@ app.use(express.static('./public/'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-const CHAMPION_KEYS = 'static-data/v3/champions?locale=en_US&tags=keys&dataById=true&';
-
-let LIST;
-
-fetch('https://na1.api.riotgames.com/lol/static-data/v3/champions?locale=en_US&tags=keys&dataById=true&api_key=RGAPI-85aa4d73-d165-421f-bdeb-eba95cde92fc')
-  .then(res => res.json())
-  .then(json => {
-    LIST = json;
-    // console.log(LIST);
+let championList;
+function getChampData() {
+  fetch(`${BASE_API_URL}${CHAMPIONS_API_URL}?locale=en_US&tags=keys&api_key=${API_KEY}`)
+  .then(res => res.json()).then(json => {
+    championList = json.keys;
+    console.log(championList);
   });
-
-function convert(id) {
-  return LIST.keys[id];
 }
 
+const championNameById = id => championList[id];
+
 app.get('/', (req, res) => {
+  getChampData();
   res.render('index');
 });
 
@@ -42,26 +40,35 @@ app.post('/search', (req, res) => {
   console.log(req.body);
   const data = {};
   const summoner = req.body.name;
-  fetch(`${API_URL}${GET_SUMMONER_URL}${summoner}?api_key=${API_KEY}`)
-    .then(res => res.json())
-    .then(json => {
-      data.summoner = json;
-      fetch(`${API_URL}match/v3/matchlists/by-account/${data.summoner.accountId}/recent?api_key=${API_KEY}`)
-        .then(res => res.json())
-        .then(json => {
-          // console.log(json);
-          data.matches = json;
-          // console.log(data);
+  let gameid;
 
-          data.matches.matches.forEach((match,index) => {
-            data.matches.matches[index].char = convert(match.champion);
-            console.log(match);
-          });
-          // console.log(data);
+  fetch(`${BASE_API_URL}${SUMMONER_ENDPOINT}${summoner}?api_key=${API_KEY}`).then(res => res.json()).then(json => {
+    data.summoner = json;
+    fetch(`${BASE_API_URL}match/v3/matchlists/by-account/${data.summoner.accountId}/recent?api_key=${API_KEY}`).then(res => res.json()).then(json => {
+      // console.log(json);
+      data.matches = json;
+      // console.log(data);
+      gameid = 2626221414; //json.matches.matches[0].gameId;
+      data.matches.matches.forEach((match, index, array) => {
 
-          res.render('search', data);
-        });
-    });
+        array[index].char = championNameById(match.champion);
+        // console.log(match);
+        if (match.queue <= 440 && match.queue >= 400) {
+          array[index].relevant = true;
+        }
+
+      });
+
+      // console.log(data);
+    }).then(() => {
+      fetch(`https://na1.api.riotgames.com/lol/match/v3/matches/2626221414?api_key=${API_KEY}`).then(res => res.json()).then(json => {
+        data.game = json;
+        console.log(data);
+        res.render('search', data);
+      });
+    })
+
+  });
 });
 
 app.listen(3000, () => console.log('Successfully started express application!'));
