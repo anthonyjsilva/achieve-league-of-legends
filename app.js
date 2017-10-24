ff195b53173a24ca6ae9fcb1063e735fe7e8798f
Anthony Silva <1xajsilva@gmail.com>
2017-10-24 15:02:06 -0500

    refactored app.js code

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 const SUMMONER_ENDPOINT = name => `${BASE_API_URL}summoner/v3/summoners/by-name/${name}?api_key=${API_KEY}`;
 const MATCHLIST_ENDPOINT = id => `${BASE_API_URL}match/v3/matchlists/by-account/${id}/recent?api_key=${API_KEY}`;
-const MATCH_ENDPOINT = id => `${BASE_API_URL}match/v3/matches/2626221414?api_key=${API_KEY}`;
+const MATCH_ENDPOINT = gameId => `${BASE_API_URL}match/v3/matches/${gameId}?api_key=${API_KEY}`;
 
 // routes ---------------------------------------------------
 
@@ -42,32 +42,32 @@ find stats on those recent games on a per-game basis
 // TODO: get the current version for static data with /realms endpoint
 app.post('/search', (req, res) => {
 
-  const data = {};
   const name = req.body.name;
-  let gameid;
-  // data.summoner.accountId
-  fetch(SUMMONER_ENDPOINT(name)).then(res => res.json()).then(json => {
-    data.summoner = json;
+  const summoner = {
+    accountInfo: "",
+    recentGames: ""
+  };
 
-    fetch(MATCHLIST_ENDPOINT(data.summoner.accountId)).then(res => res.json()).then(json => {
-      Object.assign(data, json);
-
-      // TODO: make gameid dynamic
-      gameid = 2626221414;
-
-      data.matches.forEach((match, index, array) => {
-        array[index].championName = CHAMPION_DATA.getName(match.champion);
-
-        // only show flex 5v5, solo/duo 5v5, draft and blindpick 5v5
-        // array[index].relevant = (match.queue <= 440 && match.queue >= 400);
-        array[index].relevant = (index < 5);
+  // get summoner info
+  fetch(SUMMONER_ENDPOINT(name)).then(res => res.json()).then(data => {
+    summoner.accountInfo = data;
+  }).then(() => {
+    fetch(MATCHLIST_ENDPOINT(summoner.accountInfo.accountId)).then(res => res.json()).then(data => {
+      // filter recent matches to only store matches we care about
+      summoner.recentGames = data.matches.filter((match, index, array) => match.queue <= 440 && match.queue >= 400 && index < 5);
+      summoner.recentGames.forEach((match, index, array) => {
+        match.championName = CHAMPION_DATA.getName(match.champion);
       });
     }).then(() => {
-      fetch(MATCH_ENDPOINT()).then(res => res.json()).then(json => {
-        Object.assign(data, json);
-        console.log(data);
-        res.render('search', data);
+      summoner.recentGames.forEach((match, index, array) => {
+        fetch(MATCH_ENDPOINT(match.gameId)).then(res => res.json()).then(data => {
+          console.log(data);
+          match.gameData = data;
+        });
       });
+    }).then(() => {
+      res.render('search', summoner);
+      // res.json(summoner);
     });
   });
 });
